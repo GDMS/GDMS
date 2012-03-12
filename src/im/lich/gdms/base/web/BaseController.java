c2ed471617fd3e8c1ca70eca05bfb442a0700149
LICH <lich@lich.im>
2012-03-13 03:07:33 +0800

    调整BaseController基类

--- src/im/lich/gdms/base/web/BaseController.java
+++ src/im/lich/gdms/base/web/BaseController.java
@@ -1,10 +1,7 @@
 package im.lich.gdms.base.web;
 
-import org.springframework.stereotype.Controller;
-
 import im.lich.gdms.base.BaseObject;
 
-@Controller
 public class BaseController extends BaseObject {
 
 }
