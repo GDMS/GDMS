f0373f23d82268195823e86aa04fcb7b9bd58179
LICH <lich@lich.im>
2012-03-13 12:42:37 +0800

    重写BaseObject基类的toString方法，方便显示类内部属性值

--- src/im/lich/gdms/base/BaseObject.java
+++ src/im/lich/gdms/base/BaseObject.java
@@ -1,8 +1,16 @@
 package im.lich.gdms.base;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BaseObject {
+	// 引入日志记录器
 	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	// 重写标准的toString生成
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.reflectionToString(this);
+	}
 }
