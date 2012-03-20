111434b0bbc10d005f4289913acaf876dc57689b
LICH <lich@lich.im>
2012-03-20 09:58:01 +0800

    去除多余import

--- src/im/lich/gdms/base/service/BaseService.java
+++ src/im/lich/gdms/base/service/BaseService.java
@@ -1,9 +1,5 @@
 package im.lich.gdms.base.service;
 
-import javax.annotation.Resource;
-
-import org.springframework.context.ApplicationContext;
-
 import im.lich.gdms.base.BaseObject;
 
 public class BaseService extends BaseObject {
